00c358fe02eae1aafa85129c66924d118fef030a
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-01 05:26:32 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,5 +2,9 @@ class Chatty_student < Student
   
   def hello(greeting)
     super
-    "long chaty phrase"
+    "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+  end 
   
+  def raise_hand 
+    super 
+    10.times do g
