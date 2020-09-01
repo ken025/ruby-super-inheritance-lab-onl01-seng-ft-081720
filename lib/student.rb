568f57ff83c70a9fd85860ae20be16ca34ffdd3e
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-01 05:30:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,10 @@
-class Student 
-  
+class Student
+
   def hello
-    "Hey there! I'm so excited to learn stuff.\n"
-  end 
-  
-  def raise_hand 
-    "Pick me!"
-  end 
-end 
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+
+  def raise_hand
+    puts "Pick me!"
+  end
+end
