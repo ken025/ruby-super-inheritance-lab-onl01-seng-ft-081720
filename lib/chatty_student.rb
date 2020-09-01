fb63a9d17dc83ced22a94b1249b35a88b2336f99
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-01 05:29:02 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,11 +2,11 @@ class Chatty_student < Student
   
   def hello(greeting)
     super
-    "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+    "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end 
   
   def raise_hand 
     super 
-    10.times {puts "Pick me!"}
+    10.times {super}
   end 
 end 
